1d417fb5f54da9ed73d0b446512de6cae5496ce6
InderdeepSync <buntycool1998@gmail.com>
2022-10-27 16:04:38 -0400

    Minimum Size Subarray Sum

---
+++ sliding_window/minimum_size_subarray_sum.py
@@ -0,0 +1,35 @@
+import math
+from typing import List
+
+
+def min_size_subarray_length(target: int, nums: List[int]) -> int:
+    start, end = 0, 1
+
+    min_length = math.inf
+    prev_sum = None
+    while end <= len(nums):
+        if prev_sum is None:
+            current_sum = sum(nums[start: end])
+            if current_sum >= target:
+                return 1
+
+            end += 1
+        else:
+            if prev_sum >= target:
+                current_sum = prev_sum - nums[start - 1]
+            else:
+                current_sum = prev_sum + nums[end - 1]
+
+            if current_sum >= target:
+                min_length = min(min_length, end - start)
+                start += 1
+            else:
+                end += 1
+
+        prev_sum = current_sum
+
+    return min_length if min_length != math.inf else 0
+
+
+if __name__ == "__main__":
+    print(min_size_subarray_length(7, [2, 3, 1, 2, 4, 3]))
